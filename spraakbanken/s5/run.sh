209db71ec6defab98dec06946f77a52f07c403ca
Peter Smit <peter@smitmail.eu>
2016-03-12 09:57:45 +0200

    Add AM-training to cgn and spraakbanken

--- spraakbanken/s5/run.sh
+++ spraakbanken/s5/run.sh
@@ -35,3 +35,12 @@ done
 
 utils/prepare_lang.sh data/${lang}/dict "<UNK>" data/${lang}/local/lang data/${lang}/lang
 
+steps/train_mono.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/train data/lang exp/mono0a || error_exit "Train mono failed";
+steps/align_si.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/train data/lang exp/mono0a exp/mono0a_ali || error_exit "Align mono failed";
+
+steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" 2000 10000 data/train data/lang exp/mono0a_ali exp/tri1 || error_exit "Triphone-delta failed";
+steps/align_si.sh --nj 10 --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_ali || error_exit "Align tri failed";
+
+steps/train_deltas.sh --cmd "$train_cmd" 2500 15000 data/train data/lang exp/tri1_ali exp/tri2a || error_exit "delta-delta training failed";
+steps/train_lda_mllt.sh --cmd "$train_cmd" --splice-opts "--left-context=3 --right-context=3" 2500 15000 data/train data/lang exp/tri1_ali exp/tri2b || error_exit "lda_mllt training failed";
+steps/align_si.sh  --nj 10 --cmd "$train_cmd" --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali || "Align tri2b failed";
