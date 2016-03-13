b9243e17e24c5b06a7794dbfe038c7034909f9b1
Peter Smit <peter@smitmail.eu>
2016-03-13 13:28:06 +0200

    Don't forget lang variables

--- spraakbanken/s5/run.sh
+++ spraakbanken/s5/run.sh
@@ -27,20 +27,20 @@ mfccdir=mfcc/${lang}
 
 for set in "train" "test"; do
  steps/make_mfcc.sh --cmd "${train_cmd}" --nj 20 \
-   data/${lang}/${set} exp/make_mfcc/${lang}/${set} ${mfccdir} || error_exit "make_mfcc failed";
- steps/compute_cmvn_stats.sh data/${lang}/${set} exp/make_mfcc/${lang}/${set} ${mfccdir} || error_exit "compute cmvn failed";
+   data/${lang}/${set} exp/${lang}/make_mfcc/${lang}/${set} ${mfccdir} || error_exit "make_mfcc failed";
+ steps/compute_cmvn_stats.sh data/${lang}/${set} exp/${lang}/make_mfcc/${lang}/${set} ${mfccdir} || error_exit "compute cmvn failed";
 
  utils/validate_data_dir.sh data/${lang}/${set} || error_exit "Directory data/${lang}/${set} was not properly set up"
 done
 
 utils/prepare_lang.sh data/${lang}/dict "<UNK>" data/${lang}/local/lang data/${lang}/lang
 
-steps/train_mono.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/train data/lang exp/mono0a || error_exit "Train mono failed";
-steps/align_si.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/train data/lang exp/mono0a exp/mono0a_ali || error_exit "Align mono failed";
+steps/train_mono.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/${lang}/train data/${lang}/lang exp/${lang}/mono0a || error_exit "Train mono failed";
+steps/align_si.sh --boost-silence 1.25 --nj 10 --cmd "$train_cmd" data/${lang}/train data/${lang}/lang exp/${lang}/mono0a exp/${lang}/mono0a_ali || error_exit "Align mono failed";
 
-steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" 2000 10000 data/train data/lang exp/mono0a_ali exp/tri1 || error_exit "Triphone-delta failed";
-steps/align_si.sh --nj 10 --cmd "$train_cmd" data/train data/lang exp/tri1 exp/tri1_ali || error_exit "Align tri failed";
+steps/train_deltas.sh --boost-silence 1.25 --cmd "$train_cmd" 2000 10000 data/${lang}/train data/${lang}/lang exp/${lang}/mono0a_ali exp/${lang}/tri1 || error_exit "Triphone-delta failed";
+steps/align_si.sh --nj 10 --cmd "$train_cmd" data/${lang}/train data/${lang}/lang exp/${lang}/tri1 exp/${lang}/tri1_ali || error_exit "Align tri failed";
 
-steps/train_deltas.sh --cmd "$train_cmd" 2500 15000 data/train data/lang exp/tri1_ali exp/tri2a || error_exit "delta-delta training failed";
-steps/train_lda_mllt.sh --cmd "$train_cmd" --splice-opts "--left-context=3 --right-context=3" 2500 15000 data/train data/lang exp/tri1_ali exp/tri2b || error_exit "lda_mllt training failed";
-steps/align_si.sh  --nj 10 --cmd "$train_cmd" --use-graphs true data/train data/lang exp/tri2b exp/tri2b_ali || "Align tri2b failed";
+steps/train_deltas.sh --cmd "$train_cmd" 2500 15000 data/${lang}/train data/${lang}/lang exp/${lang}/tri1_ali exp/${lang}/tri2a || error_exit "delta-delta training failed";
+steps/train_lda_mllt.sh --cmd "$train_cmd" --splice-opts "--left-context=3 --right-context=3" 2500 15000 data/${lang}/train data/${lang}/lang exp/${lang}/tri1_ali exp/${lang}/tri2b || error_exit "lda_mllt training failed";
+steps/align_si.sh  --nj 10 --cmd "$train_cmd" --use-graphs true data/${lang}/train data/${lang}/lang exp/${lang}/tri2b exp/${lang}/tri2b_ali || "Align tri2b failed";
