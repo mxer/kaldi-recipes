767cc4cbaf91a0ab16ca54e1d01618ffe1f1dd68
Peter Smit <peter@smitmail.eu>
2016-03-14 16:08:55 +0200

    Fix lexicon item for UNK

--- spraakbanken/s5/spr_local/spr_pron_to_std.py
+++ spraakbanken/s5/spr_local/spr_pron_to_std.py
@@ -59,7 +59,7 @@ def map_transcript(trans):
 
 
 def transform_lexicon(input, output, phone_list, question_list):
-    d = {"<UNK>": [["NSN"]], }
+    d = {"<UNK>": [("NSN",)], }
 
     for line in input:
         if ";" not in line:
