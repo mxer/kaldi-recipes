5f9ce441361e4deae683e380de49fa7b1c9f4930
Peter Smit <peter@smitmail.eu>
2016-03-23 17:14:06 +0200

    New extraction script

---
+++ spraakbanken/s5/spr_local/corpus_to_kaldi_format.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import collections
+import os
+import subprocess
+import sys
+
+import spl
+
+
+def main(in_dir, out_scp, out_dir):
+    err_counter = collections.Counter()
+
+    wav_files = {}
+    spl_files = {}
+
+    for root, dirs, files in os.walk(os.path.normpath(in_dir)):
+        parts = root.split(os.sep)
+
+        for f in files:
+            if f.endswith(".wav") and f.startswith("u"):
+                key = parts[-2] + os.path.splitext(f)[0]
+                wav_files[key.lower()] = os.path.join(root, f)
+
+            if f.endswith(".spl"):
+                key = parts[-1] + os.path.splitext(f)[0]
+                spl_files[key.lower()] = os.path.join(root, f)
+
+    fd_text = open(os.path.join(out_dir, 'text'), 'w', encoding='utf-8')
+    fd_scp = open(out_scp, 'w', encoding='utf-8')
+    fd_utt2spk = open(os.path.join(out_dir, 'utt2spk'), 'w', encoding='utf-8')
+    fd_utt2type = open(os.path.join(out_dir, 'utt2type'), 'w', encoding='utf-8')
+
+    for key, val in spl_files.items():
+        s = spl.Spl(val)
+        for valid, record in s.records():
+
+            spl_wav_filename = os.path.splitext(valid[9])[0]
+            wav_key = key[:8] + spl_wav_filename
+            wav_key = wav_key.lower()
+
+            utt_type = record[9]
+            utt_text = " ".join(valid[0].split())
+            file_name = wav_files[wav_key]
+
+            if os.stat(file_name).st_size == 0:
+                err_counter["File empty error"] += 1
+                print("{} is empty".format(file_name))
+                continue
+            try:
+                num_sam = int(subprocess.check_output("soxi -s {}".format(file_name), stderr=subprocess.STDOUT, shell=True))
+            except subprocess.CalledProcessError:
+                err_counter["Reading file error"] += 1
+                continue
+
+            if num_sam * 4 != int(valid[11]) - int(valid[10]):
+                err_counter["Length incorrect error"] += 1
+                continue
+
+            for channel in ("1", "2"):
+                utt_key = "{}-{}-{}-{}".format(key[:8], spl_wav_filename[1:5], spl_wav_filename[5:], channel)
+
+                print("{} sph2pipe -f wav -p -c {} {} |".format(utt_key, channel, file_name), file=fd_scp)
+                print("{} {}".format(utt_key, utt_text), file=fd_text)
+                print("{} {}".format(utt_key, utt_key[:13]), file=fd_utt2spk)
+                print("{} {}".format(utt_key, utt_type), file=fd_utt2type)
+
+    for type, count in err_counter.most_common():
+        print("{} errors of type {} occured".format(count, type), file=sys.stderr)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        exit("3 required arguments: source data directory, output wav file, output dir")
+
+    main(*sys.argv[1:])
---
+++ spraakbanken/s5/spr_local/spr_dp_corpus.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+LC_ALL=C
+
+if [ -d data-prep/corpus ]; then
+    ok=0
+    for f in "wav.ark" "wav.scp" "text" "utt2spk" "utt2set" "utt2type"; do
+        if [ ! -f data-prep/corpus/$f ]; then
+            ok=1
+        fi
+    done
+
+    if [ $ok -eq 0 ]; then
+        echo "There seems to be a corpus in data-prep corpus, so we assume we don't need the original corpus files. If data preparation fails, remove data-prep/corpus and try again"
+        exit 0
+    fi
+    rm -Rf data-prep/corpus
+fi
+
+echo $(date) "Check the integrity of the archives"
+wd=$(pwd)
+(cd corpus && md5sum -c ${wd}/local/checksums/corpus) || exit "The spraakbankenarchives gave unexpected md5 sums"
+
+raw_files_dir=$(mktemp -d)
+data_dir=$(mktemp -d)
+
+echo "Temporary directories (should be cleaned afterwards):" ${raw_files_dir} ${data_dir}
+
+echo $(date) "Start untarring"
+for t in $(cut -f3- -d" " local/checksums/${set}); do
+    tar xzf corpus/${t} --strip-components=3 -C ${raw_files_dir}
+done
+
+echo $(date) "Read the corpus"
+spr_local/corpus_to_kaldi_format.py ${raw_files_dir} ${raw_files_dir}/wav.scp ${data_dir}
+
+mkdir -p data-prep/corpus
+
+for f in $(ls -1 ${data_dir}); do
+    sort < ${data_dir}/${f} > data-prep/corpus/${f}
+done
+
+sort < ${raw_files_dir}/wav.scp > ${raw_files_dir}/sorted.scp
+
+
+echo $(date) "Start wav-copy"
+wav-copy scp:${raw_files_dir}/sorted.scp ark,scp:data-prep/corpus/wav.ark,data-prep/corpus/wav.scp
+
+echo $(date) "Start removing temp dirs"
+rm -Rf ${data_dir} ${raw_files_dir}
