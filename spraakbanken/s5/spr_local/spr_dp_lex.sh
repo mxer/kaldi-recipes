4dc7555a73347ac297f454af9e556169d53f03c6
Peter Smit <peter.smit@aalto.fi>
2016-04-19 10:47:03 +0200

    Make different lexes

--- spraakbanken/s5/spr_local/spr_dp_lex.sh
+++ spraakbanken/s5/spr_local/spr_dp_lex.sh
@@ -16,7 +16,7 @@ echo "$0 $@"  # Print the command line for logging
 [ -f path.sh ] && . ./path.sh # source the path.
 . parse_options.sh || exit 1;
 
-if [ $# != 2 ]; then
+if [ $# != 1 ]; then
    echo "usage: spr_local/spr_dp_lex.sh out_dir"
    echo "e.g.:  steps/spr_dp_lex.sh --lowercase true --accents false data-prep/lexicon_lc_na"
    echo "main options (for others, see top of script file)"
@@ -56,7 +56,7 @@ opt2="noaccent"
 if $lowercase; then
     opt1="lowercase"
 fi
-if $accent; then
+if $accents; then
     opt2="accent"
 fi
 
