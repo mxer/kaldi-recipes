a15b0aa7026305d7daf557e9e6f04c11ad0ec748
Peter Smit <peter.smit@aalto.fi>
2016-05-04 15:18:56 +0300

    Polished scripts for lc_na lex recognition

--- spraakbanken/s5/decode120.sh
+++ spraakbanken/s5/decode120.sh
@@ -16,8 +16,8 @@ mfccdir=mfcc
 numjobs=10
 
 dataset=dev
-lm=20k_2gram
-biglm=20k_5gram
+lm=120k_2gram
+biglm=120k_5gram
 
 numjobs=$(cat data/${dataset}/spk2utt | wc -l)
 echo "Changing numjobs to ${numjobs}"
@@ -31,7 +31,7 @@ job mkg_${am} 26 40 split_data \
 job dec_${am} 6 40 LAST \
  -- steps/decode.sh --nj ${numjobs} --cmd "$decode_cmd" exp/${am}/graph_${lm} data/${dataset} exp/${am}/decode_${lm}_${dataset}
 job dec_bl_${am} 6 40 mkg_${am} \
- -- steps/decode_biglm.sh --nj ${numjobs} --cmd "$decode_cmd" exp/${am}/graph_${lm} data/${lm}/G.fst data/${biglm}/G.fst data/${dataset} exp/${am}/decode_${lm}_bl_${biglm}
+ -- steps/decode_biglm.sh --nj ${numjobs} --cmd "$big_decode_cmd" exp/${am}/graph_${lm} data/${lm}/G.fst data/${biglm}/G.fst data/${dataset} exp/${am}/decode_${lm}_bl_${biglm}
 job dec_rs_${am} 6 40 dec_${am} \
  -- steps/lmrescore.sh --cmd "$decode_cmd" data/${lm} data/${biglm} data/${dataset} exp/${am}/decode_${lm}_${dataset} exp/${am}/decode_${lm}_rs_${biglm}
 prev=$am
@@ -41,7 +41,7 @@ for am in "tri1" "tri2a" "tri2b"; do
     job dec_${am} 6 40 LAST \
      -- steps/decode.sh --nj ${numjobs} --cmd "$decode_cmd" exp/${am}/graph_${lm} data/${dataset} exp/${am}/decode_${lm}_${dataset}
     job dec_bl_${am} 6 40 mkg_${am} \
-     -- steps/decode_biglm.sh --nj ${numjobs} --cmd "$decode_cmd" exp/${am}/graph_${lm} data/${lm}/G.fst data/${biglm}/G.fst data/${dataset} exp/${am}/decode_${lm}_bl_${biglm}
+     -- steps/decode_biglm.sh --nj ${numjobs} --cmd "$big_decode_cmd" exp/${am}/graph_${lm} data/${lm}/G.fst data/${biglm}/G.fst data/${dataset} exp/${am}/decode_${lm}_bl_${biglm}
     job dec_rs_${am} 6 40 dec_${am} \
      -- steps/lmrescore.sh --cmd "$decode_cmd" data/${lm} data/${biglm} data/${dataset} exp/${am}/decode_${lm}_${dataset} exp/${am}/decode_${lm}_rs_${biglm}
     prev=$am
