931b44d75587ee7cd5c83a53d26fa4aa83789d69
Peter Smit <peter.smit@aalto.fi>
2016-06-14 14:24:56 +0300

    wip

---
+++ spraakbanken/s5/spr_local/smart_morfessor_segment.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import argparse
+
+import morfessor
+
+def main(model, infile, outfile):
+
+    io = morfessor.MorfessorIO()
+    m = io.read_any_model(model)
+    with open(outfile, 'w', encoding='utf-8') as out_f:
+        for line in open(infile, encoding='utf-8'):
+            parts = line.split()
+            np = []
+
+            for p in parts:
+                if any(c in "<>." for c in p):
+                    np.append(p)
+                else:
+                    np.append(" ".join(m.viterbi_segment(p)[0]))
+            result = " ".join(np)
+
+            print(result, file=out_f)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Segment corpus, but not tokens with punctuation')
+    parser.add_argument('model')
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+
+    args = parser.parse_args()
+
+    main(args.model, args.infile, args.outfile)
--- spraakbanken/s5/spr_local/spr_make_morph_G.sh
+++ spraakbanken/s5/spr_local/spr_make_morph_G.sh
@@ -54,9 +54,10 @@ spr_local/to_lower.py < data-prep/ngram/corpus | split -l 1000000 --numeric-suff
 last=$(ls -1 $tmpcount | sort -n | tail -n1)
 mkdir $tmpcount/out
 mkdir $tmpcount/log
-$cmd JOB=1000:$last $tmpcount/log/JOB.log morfessor-segment -l $outdir/morfessor.bin \< $tmpcount/JOB \> $tmpcount/out/JOB
+#$cmd JOB=1000:$last $tmpcount/log/JOB.log morfessor-segment --encoding=utf-8 -l $outdir/morfessor.bin -o $tmpcount/out/JOB $tmpcount/JOB 
+$cmd JOB=1000:$last $tmpcount/log/JOB.log spr_local/smart_morfessor_segment.py $outdir/morfessor.bin $tmpcount/JOB $tmpcount/out/JOB 
 
-cat $tmpcount/out/* $outdir/corpus
+cat $tmpcount/out/* > $outdir/corpus
 
 
 
