0cbdde162b2661c33a867147998b542ece029e83
Peter Smit <peter@smitmail.eu>
2016-06-20 09:26:01 +0300

    Extract sonar script

---
+++ cgn/s5/local/SoNaR_to_corpus.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import argparse
+import tarfile
+
+from pynlpl.formats import folia
+
+
+def main(selection, infile, outfile):
+    with open(outfile, 'w', encoding='utf-8') as of:
+        t = tarfile.TarFile(infile, 'r|*')
+
+        while True:
+            ti = t.next()
+            if ti is None:
+                break
+
+            if not ti.name.endswith(".folia.xml") or not any(c in ti.name for c in selection):
+                continue
+
+            print("FILE: {}".format(ti.name), file=of)
+
+            for s in folia.Document(string=t.extractfile(ti).read()).sentences():
+                print(s.strip(), file=of)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Extract corpus from SoNaR')
+    parser.add_argument('selection')
+    parser.add_argument('infile')
+    parser.add_argument('outfile')
+
+    args = parser.parse_args()
+
+    main({args.selection.split("|")}, args.infile, args.outfile)
