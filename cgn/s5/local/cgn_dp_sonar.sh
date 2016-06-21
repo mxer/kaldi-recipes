3ca6d366f2b9b2993772f2459402fdac7c07ae68
Peter Smit <peter@smitmail.eu>
2016-06-21 08:35:47 +0300

    WIP

--- cgn/s5/local/cgn_dp_sonar.sh
+++ cgn/s5/local/cgn_dp_sonar.sh
@@ -2,3 +2,4 @@
 
 export LC_ALL=C
 
+tar xf ~w/c/SoNaR/20150602_SoNaRCorpus_NC_1.2.1.gz ./SoNaRCorpus_NC_1.2/SONAR500/FoLiA/WR-P-E-G_subtitles/ ./SoNaRCorpus_NC_1.2/SONAR500/FoLiA/WR-P-P-B_books/ ./SoNaRCorpus_NC_1.2/SONAR500/FoLiA/WR-P-P-G_newspapers/ ./SoNaRCorpus_NC_1.2/SONAR500/FoLiA/WS-U-E-A_auto_cues/ ./SoNaRCorpus_NC_1.2/SONAR500/FoLiA/WS-U-T-B_texts_for_the_visually_impaired/
