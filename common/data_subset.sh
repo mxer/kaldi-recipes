7ba98c0887786114af4e7fd2ccfe95bbb7dd16f4
Peter Smit <peter@smitmail.eu>
2016-06-27 13:31:18 +0300

    Output vocab

--- common/data_subset.sh
+++ common/data_subset.sh
@@ -20,4 +20,5 @@ for set in $(ls -1 definitions/selections/); do
     common/filter_audio_dir.py data-prep/audio data/${set} definitions/selections/${set}
     utils/utt2spk_to_spk2utt.pl data/${set}/utt2spk > data/${set}/spk2utt
     local/make_transcript.py data/${set} data-prep/lexicon/lexicon.txt
+    awk '{print substr($0, index($0, $1))}' < data/${set}/text | tr ' ' '\n' | sort -u > data/${set}/vocab
 done
