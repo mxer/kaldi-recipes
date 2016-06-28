4fb1ca10c776fc07c36f59575404b6260391500c
Peter Smit <peter@smitmail.eu>
2016-06-28 14:41:18 +0300

    Add corpus preprocessing

---
+++ common/preprocess_corpus.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import argparse
+import codecs
+import lzma
+import sys
+import unicodedata
+
+
+def preprocess_corpus(inf, outf):
+    for line in lzma.open(inf, 'rt', encoding='utf-8'):
+        if line.startswith("FILE"):
+            continue
+
+        sent = []
+
+        for w in line.strip().split():
+            if not any(unicodedata.category(c).startswith("L") for c in w):
+                continue
+            sent.append(w)
+
+        if sent[0] != "<s>":
+            sent = ["<s>"] + sent
+        if sent[-1] != "</s>":
+            sent.append("</s>")
+
+        print(" ".join(sent), file=outf)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'), default=sys.stdin.buffer)
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w', encoding='utf-8'), default=codecs.getwriter('utf-8')(sys.stdout.buffer))
+
+    args = parser.parse_args()
+
+    preprocess_corpus(args.infile, args.outfile)
