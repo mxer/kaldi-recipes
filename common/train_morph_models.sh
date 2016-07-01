fcdd7d19c9e6c935400b0464ec482161add0cf2b
Peter Smit <peter.smit@aalto.fi>
2016-07-01 14:46:41 +0300

    Small fixes

--- common/train_morph_models.sh
+++ common/train_morph_models.sh
@@ -5,8 +5,8 @@ echo "$0 $@"  # Print the command line for logging
 [ -f path.sh ] && . ./path.sh # source the path.
 . parse_options.sh || exit 1;
 
-if [ $# != 1 ]; then
-   echo "usage: train_word_models.sh segmentation_dir"
+if [ $# != 0 ]; then
+   echo "usage: train_word_models.sh"
    exit 1;
 fi
 
@@ -18,8 +18,8 @@ fi
 JOB_PREFIX=$(cat id)_
 
 mkdir -p data/segmentation/morph1
-job train_morfessor 10 10 NONE -- common/train_morfessor_model.sh data-prep/text/text.orig.xz 300000 data/segmentation/morph1/morfessor.bin data/segmentation/morph1/vocab
-job morfessor_segment 4 48 train_morfessor -- common/morfessor_segment.sh data-prep/text/text.orig.xz data/segmentation/morph1/morfessor.bin data/segmentation/morph1/corpus.xz
+job train_morfessor 10 24 NONE -- common/train_morfessor_model.sh data-prep/text/text.orig.xz 300000 data/segmentation/morph1/morfessor.bin data/segmentation/morph1/vocab
+job morfessor_segment 4 72 train_morfessor -- common/morfessor_segment.sh data-prep/text/text.orig.xz data/segmentation/morph1/morfessor.bin data/segmentation/morph1/corpus.xz
 
 if [ -e data/dicts/morph1 ]; then rm -Rf data/dicts/morph1; fi
 if [ -e data/langs/morph1_base ]; then rm -Rf data/langs/morph1_base; fi
@@ -37,7 +37,7 @@ for d in "0.05" "0.02" "0.01" "0.005" "0.002" "0.001"; do
 
     for order in "3" "5" "30"; do
         mkdir -p data/lm/morph1/vk/${d}_${order}g
-        job vari_${d}_${order}g 60 48 morfessor_segment -- common/train_varikn_model.sh data/segmentation/morph1/corpus.xz data/segmentation/morph1/vocab ${d} ${order} data/lm/morph1/vk/${d}_${order}g/arpa.xz
+        job vari_${d}_${order}g 45 72 morfessor_segment -- common/train_varikn_model.sh data/segmentation/morph1/corpus.xz data/segmentation/morph1/vocab ${d} ${order} data/lm/morph1/vk/${d}_${order}g/arpa.xz
 
         job recog_lang_${d}_${order}g 16 1 vari_${d}_${order}g,make_morph_lang -- common/make_recog_lang.sh data/lm/morph1/vk/${d}_${order}g/arpa.xz data/langs/morph1_base data/recog_langs/morph1_base_v_${d}_${order}gram
         job recog_lang_${d}_${order}g2 16 1 vari_${d}_${order}g,make_morph_lang -- common/make_recog_lang.sh --inwordbackoff true data/lm/morph1/vk/${d}_${order}g/arpa.xz data/langs/morph1_base data/recog_langs/morph1_base_backoff_v_${d}_${order}gram
