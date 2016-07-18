64e329818cabc959c82beaa243d156dd56d5ab7d
Peter Smit <peter@smitmail.eu>
2016-07-18 16:06:38 +0300

    WIP

---
+++ common/make_dict_fix2.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+export LC_ALL=C
+
+# Begin configuration section.
+# End configuration options.
+
+echo "$0 $@"  # Print the command line for logging
+
+[ -f path.sh ] && . ./path.sh # source the path.
+. parse_options.sh || exit 1;
+
+if [ $# != 3 ]; then
+   echo "usage: common/make_dict.sh vocab dir_out m2m_aligned"
+   echo "e.g.:  common/make_dict.sh data/train/vocab data/dict data/m2m/fix2/aligned"
+   echo "main options (for others, see top of script file)"
+   exit 1;
+fi
+
+vocab=$1
+outdir=$2
+m2m=$3
+
+tmpdir=$(mktemp -d)
+echo "Tmpdir: ${tmpdir}"
+common/make_m2m_lex.py ${m2m} ${vocab} ${tmpdir}/found.lex ${tmpdir}/oov
+
+phonetisaurus-g2pfst --print_scores=false --model=data-prep/lexicon/g2p_wfsa --wordlist=${tmpdir}/oov | sed "s/\t$/\tSIL/" > ${tmpdir}/oov.lex
+
+mkdir -p ${outdir}
+cat ${tmpdir}/found.lex ${tmpdir}/oov.lex definitions/dict_prep/lex | sort -u > ${outdir}/lexicon.txt
+
+echo "SIL" > ${tmpdir}/silence_phones.txt
+cut -f2 definitions/dict_prep/lex >> ${tmpdir}/silence_phones.txt
+sort -u < ${tmpdir}/silence_phones.txt > ${outdir}/silence_phones.txt
+
+echo "SIL" > ${outdir}/optional_silence.txt
+
+cut -f2- < ${outdir}/lexicon.txt | tr ' ' '\n' | sed 's/^[ \t]*//;s/[ \t]*$//' | sed '/^$/d' | sort -u | grep -v -F -f ${outdir}/silence_phones.txt > ${outdir}/nonsilence_phones.txt
+
+rm -Rf ${tmpdir}
--- common/make_m2m_lex.py
+++ common/make_m2m_lex.py
@@ -3,14 +3,23 @@
 import argparse
 
 
-def main(m2m, vocab, outlex):
+def main(m2m, vocab, outlex, oovlist):
     alignment = {}
     for line in m2m:
         l,r = line.strip().split("\t", 1)
         l_parts = tuple(x for x in l.replace("+", "").split("|") if len(x) > 0)
         r_parts = tuple(tuple(x.split("+")) for x in r.replace("+", "").split("|") if len(x) > 0 )
 
-        alignment[]
+        alignment[''.join(l_parts)] = (l_parts, r_parts)
+
+    for v in vocab:
+        transcriptions = set()
+        si = 1 if v.startswith('+') else 0
+        se = len(v)-1 if v.endswith('+') else len(v)
+
+        for k, parts in alignment.items():
+            if v in k:
+                pass #todo
 
 
 
@@ -20,6 +29,9 @@ if __name__ == "__main__":
     parser.add_argument('inm2m', type=argparse.FileType('r', encoding='utf-8'))
     parser.add_argument('invocab', type=argparse.FileType('r', encoding='utf-8'))
     parser.add_argument('outlex', type=argparse.FileType('w', encoding='utf-8'))
+    parser.add_argument('oovlist', type=argparse.FileType('w', encoding='utf-8'))
     args = parser.parse_args()
 
-    main(args.inm2m, args.invocab, args.outlex)
+
+
+    main(args.inm2m, args.invocab, args.outlex, args.oovlist)
