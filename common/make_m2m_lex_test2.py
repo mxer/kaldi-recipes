16aaa8ba62f85fb0b91a4bc90bdf73dc76ce5a7a
Peter Smit <peter@smitmail.eu>
2016-08-01 10:30:05 +0300

    WIP

--- common/make_m2m_lex_test2.py
+++ common/make_m2m_lex_test2.py
@@ -8,7 +8,7 @@ import itertools
 import sys
 
 
-def main(m2m, vocab, outlex, oovlist):
+def main(m2m, morph_list, outlex, oovlist):
     alignment = {}
     for line in m2m:
         l,r = line.strip().split("\t", 1)
@@ -20,34 +20,17 @@ def main(m2m, vocab, outlex, oovlist):
         alignment[''.join(l_strings)] = (l_indexes, r_parts)
 
     print("m2m loaded", file=sys.stderr)
-    for v in vocab:
-        v = v.strip()
-        print("word {}".format(v), file=sys.stderr)
-        transcriptions = set()
-        #
-        # si = 1 if v.startswith('+') else 0
-        # se = len(v)-1 if v.endswith('+') else len(v)
+    for v in morph_list:
+        word = v.strip().replace("+ +", "")
+        if word not in alignment:
+            print(v.strip(), file=oovlist)
+            continue
 
-        w = v.strip("+")
-
-        for k, parts in alignment.items():
-
-            if w not in k:
-                continue
-
-            if not v.startswith('+'):
-                if not k.startswith(w):
-                    continue
-                si = 0
-                ei = len(w)
-            elif not v.endswith('+'):
-                if not k.endswith(w):
-                    continue
-                si = len(k) - len(w)
-                ei = len(k)
-            else:
-                si = k.index(w)
-                ei = si + len(w)
+        parts = alignment[word]
+        si = 0
+        for m in v.strip().split():
+            cm = m.strip("+")
+            ei = si + len(cm)
 
             sp = bisect.bisect_left(parts[0], si)
             if si == parts[0][sp]:
@@ -63,18 +46,13 @@ def main(m2m, vocab, outlex, oovlist):
 
             for s, e in itertools.product(possible_starts, possible_ends):
                 if e > s:
-                    transcriptions.add(tuple(itertools.chain(*parts[1][s:e])))
-
-        if len(transcriptions) == 0:
-            print(v, file=oovlist)
-        else:
-            for t in transcriptions:
-                print("{} {}".format(v, " ".join(t)), file=outlex)
+                    print("".format(m, " ".join(itertools.chain(*parts[1][s:e]))), file=outlex)
+            si = ei
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('inm2m', type=argparse.FileType('r', encoding='utf-8'))
-    parser.add_argument('invocab', type=argparse.FileType('r', encoding='utf-8'))
+    parser.add_argument('inmorph', type=argparse.FileType('r', encoding='utf-8'))
     parser.add_argument('outlex', type=argparse.FileType('w', encoding='utf-8'))
     parser.add_argument('oovlist', type=argparse.FileType('w', encoding='utf-8'))
     args = parser.parse_args()
