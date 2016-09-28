ef327411489c65ffe47917cf84a760ac22e27f8c
Peter Smit <peter@smitmail.eu>
2016-09-28 10:41:05 +0300

    OOV rate tool

---
+++ common/find_oov_rate.py
@@ -0,0 +1,25 @@
+import argparse
+
+
+def main(vocab, text):
+    vocab = set(l.strip().split()[0] for l in vocab)
+
+    count = 0
+    count_oov = 0
+    for line in text:
+        for word in line.strip().split()[1:]:
+            count += 1
+            if word not in vocab:
+                count_oov += 1
+
+    print("OOV rate {}".format(float(count_oov) / float(count)))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vocab', type=argparse.FileType('r', encoding='utf-8'))
+    parser.add_argument('text', type=argparse.FileType('r', encoding='utf-8'))
+
+    args = parser.parse_args()
+
+    main(args.vocab, args.text)
